159fd5fda644b400a297f5368d9ecbb194fdf0e6
nignxuehua <huaningxue00@gmail.com>
2025-04-20 22:38:31 +0800

    Update build.sh
    添加网易云音乐解锁、smartdns、定时重启

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -40,7 +40,10 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-
+#------自定义组件----------
+PACKAGES="$PACKAGES luci-app-unblockneteasemusic"
+PACKAGES="$PACKAGES luci-i18n-advanced-reboot"
+PACKAGES="$PACKAGES luci-app-smartdns"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
